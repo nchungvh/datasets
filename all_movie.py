d88faa881c5a48ae48f07d266cfbb1009d971c56
Chungnt <ntchung1998@gmail.com>
2019-07-30 01:23:29 +0700

    .

--- all_movie.py
+++ all_movie.py
@@ -6,16 +6,20 @@ with open('all_movie.csv', "r") as lst:
     file = lst.readlines()
 cast = []
 for i in range(len(file)-1):
-    cast.append(np.array(file[i+1].split(','))[:6])
+    dem =0
+    for k in range(6):
+        dem =k
+        if((file[i+1].split(','))[k] == 'Cast Not Available'):
+            break
+    cast.append(np.array(file[i+1].split(','))[:dem])
 cast = np.asarray(cast)
-print(cast[1][5])
 edges = []
 for i in range(len(cast)-1):
     print(i)
     for j in range(i+1,len(cast)):
-        for m in range(6):
-            for n in range(6):
-                if((cast[i][m]==cast[j][n])&(str(cast[i][m])!='Cast Not Available')):
+        for m in range(len(cast[i])):
+            for n in range(len(cast[j])):
+                if(cast[i][m]==cast[j][n]):
                     edges.append(np.array([i+2,j+2]))
 edges = np.asarray(edges)
 output = open('edges_all_movie.pkl', 'wb')
